8ed975f2ff61bf98a2c2c8882b189ca773bb8e2c
ShizuheK <phkaiisr@gmail.com>
2023-04-15 12:14:07 -0300

    Add basic file structure

---
+++ xti/src/main/java/com/mycompany/xti/Xti.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.xti;
+
+/**
+ *
+ * @author k
+ */
+public class Xti {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
