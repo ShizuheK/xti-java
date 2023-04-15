c624637c083ef97a124788ae16ce795a172a7a0b
ShizuheK <phkaiisr@gmail.com>
2023-04-15 12:54:39 -0300

    Add external package test for Dog

---
+++ xti/src/main/java/com/mycompany/xti/DogExternalTest.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.xti;
+import br.com.xti.oop.Dog;
+/**
+ *
+ * @author k
+ */
+public class DogExternalTest {
+    public static void main(String[] args){
+        Dog mirio = new Dog();
+    }
+}
