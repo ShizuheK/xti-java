4380a1a1a5c30905323fc857a7690af592283c8d
ShizuheK <phkaiisr@gmail.com>
2023-04-16 22:06:13 -0300

    Add AnimalTest class

---
+++ xti/src/main/java/br/com/xti/inheritance/AnimalTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.xti.inheritance;
+
+/**
+ *
+ * @author k
+ */
+public class AnimalTest {
+    
+    public static void main(String[] args){
+        
+        
+        
+    }
+    
+}
