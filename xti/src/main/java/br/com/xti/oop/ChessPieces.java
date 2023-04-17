0bdb6571f2080b784a9f1ea4f5143caf5549d39b
ShizuheK <phkaiisr@gmail.com>
2023-04-16 21:38:50 -0300

    Add EnumTest class and ChessPieces enum

---
+++ xti/src/main/java/br/com/xti/oop/ChessPieces.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package br.com.xti.oop;
+
+/**
+ *
+ * @author k
+ */
+public enum ChessPieces {
+    
+    KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN;
+    
+}
---
+++ xti/src/main/java/br/com/xti/oop/EnumTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.xti.oop;
+
+/**
+ *
+ * @author k
+ */
+public class EnumTest {
+    
+    public static final double PI = 3.14;
+    
+    public static void main(String[] args){
+        
+        
+        
+    }
+    
+}
