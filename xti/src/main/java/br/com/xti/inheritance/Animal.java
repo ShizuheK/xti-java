07d9a8b5784a80dc2c948e0c18e4722c8e1096c2
ShizuheK <phkaiisr@gmail.com>
2023-04-16 22:05:08 -0300

    Add Inheritance Package with two classes and one superclass

---
+++ xti/src/main/java/br/com/xti/inheritance/Animal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.xti.inheritance;
+
+/**
+ *
+ * @author k
+ */
+public class Animal {
+    
+    double weight;
+    String food;
+    
+    void sleep(){System.out.println("Sleeping...");}
+    
+    void watch(){System.out.println("Watching...");}
+    
+}
